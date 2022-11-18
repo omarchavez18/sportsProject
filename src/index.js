b523300a05a5a520f1631dfabb506a82a3e76288
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-11-17 20:51:03 -0600

    new sections imported

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./style.css";
 import { createHome, mainHome, footer } from "./home";
 import { mainSports } from "./sports";
 createHome();
-mainHome();
-// footer();
+//mainHome();
 mainSports();
+footer();
