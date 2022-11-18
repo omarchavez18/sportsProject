58a8725d8331d5f927cbb85f915058aea8ebb56b
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-11-17 22:44:12 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./style.css";
 import { createHome, mainHome, footer } from "./home";
 import { mainSports } from "./sports";
+import { mainContact } from "./contact";
 createHome();
-//mainHome();
-mainSports();
+mainHome();
+//mainSports();
+//mainContact();
 footer();
