fa201f20667112cb5042b992ce0784d8dc7c008a
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-11-24 19:49:53 -0600

    function to delete id´s that do not match

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import "./style.css";
-import { createHome, mainHome, footer } from "./home";
+import { createHome, mainHome, footers } from "./home";
 import { mainSports } from "./sports";
 import { mainContact } from "./contact";
+
+//function to delete the main tag
+export function clearContainer(id) {
+  const globalMain = document.querySelector("main");
+
+  /* This condition is to compare the existence of a tag and its id and if its id already exists it prevents it from being DELETED,
+  if the id is different it proceeds to DELETE it*/
+  if (globalMain && globalMain.id == id) {
+    return;
+  }
+  const content = document.getElementById("content");
+  content.removeChild(globalMain);
+}
+
 createHome();
 mainHome();
-//mainSports();
-//mainContact();
-footer();
+footers();
+
+// taskbar click events
+//Home
+const home = document.querySelector(".home");
+home.addEventListener("click", () => {
+  clearContainer("mainHome");
+});
+home.addEventListener("click", mainHome);
+
+//Sports
+const sports = document.querySelector(".sports");
+sports.addEventListener("click", () => {
+  clearContainer("mainSports");
+});
+sports.addEventListener("click", mainSports);
+
+//Contact
+const contact = document.querySelector(".contact");
+contact.addEventListener("click", () => {
+  clearContainer("mainContact");
+});
+contact.addEventListener("click", mainContact);
